434304abdc55af228ca8d08cf13cb5445135a795
Rodrigo Bassi Guerreiro <rb419@duke.edu>
2024-10-29 17:07:55 -0400

    First potential touchscreen code

---
+++ pi/tft_test.py
@@ -0,0 +1,19 @@
+import board
+import busio
+import digitalio
+from adafruit_ra8875 import RA8875
+
+# Setup SPI bus and RA8875 display object
+spi = busio.SPI(clock=board.SCLK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.CE0)
+rst = digitalio.DigitalInOut(board.D22)  # Example GPIO for reset
+display = RA8875(spi, cs, rst)
+
+# Initialize display
+display.initialize()
+display.width = 800  # Set the width of your display
+display.height = 480  # Set the height of your display
+
+# Test drawing a rectangle
+display.fill(0xFFFF)  # Fill screen with white
+display.draw_rectangle(50, 50, 200, 100, 0x001F)  # Blue rectangle
